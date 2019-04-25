360d3d9bd171cc714fde08f4f642816e056c3203
patoattie <patoattie@gmail.com>
2019-04-25 20:02:29 -0300

    Clase 1

--- Clase1/Ejer1/js/scripts.js
+++ Clase1/Ejer1/js/scripts.js
@@ -1,6 +1,18 @@
+//window.onload = inicializarEventos;
+window.addEventListener('load', inicializarEventos);
+
 function cambiarColor()
 {
     var h1 = document.getElementById('miH1');
 
     h1.style.color = "blue";
+}
+
+function inicializarEventos()
+{
+    var imagen = document.getElementsByTagName('img')[0];
+    imagen.addEventListener('mouseover', function(e)
+    {
+        e.target.setAttribute('src', './images/favicon.png');
+    });
 }
