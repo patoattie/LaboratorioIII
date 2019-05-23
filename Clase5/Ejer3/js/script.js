9aec3f644a0a38251762589740b0acfb753899cb
patoattie <patoattie@gmail.com>
2019-05-23 17:55:28 -0300

    Clase 5

--- Clase5/Ejer3/js/script.js
+++ Clase5/Ejer3/js/script.js
@@ -9,14 +9,25 @@ function ejecutar()
 {
     var div = document.getElementById("miDiv");
     var primerHijo = div.firstElementChild; //Retorna el primer hijo de tipo elemento
-    //var ultimoHijo = div.lastElementChild; //Retorna el ultimo hijo de tipo elemento
+    var ultimoHijo = div.lastElementChild; //Retorna el ultimo hijo de tipo elemento
     //var hermano = primerHijo.nextElementSibling; //Retorna el proximo hermano de tipo elemento. Si no encuentra devuelve null.
 
-    while (primerHijo)
+    //console.log(primerHijo);
+    //console.log(primerHijo.childNodes[0].nodeValue); //nodeValue solamente se puede usar en nodos de tipo texto
+    //console.log(primerHijo.textContent); //valor del nodo de tipo texto que depende del nodo de tipo elemento
+
+    /*while (primerHijo)
     {
         console.log(primerHijo);
         primerHijo = primerHijo.nextElementSibling;
+    }*/
+
+    while (ultimoHijo)
+    {
+        ultimoHijo.textContent = ultimoHijo.textContent + ".......";
+        ultimoHijo = ultimoHijo.previousElementSibling;
     }
+
     //console.log(primerHijo);
     //console.log(ultimoHijo);
     //console.log(hermano);
