6680bc551ebcb13dce1e61e6a9aecf642959061c
patoattie <patoattie@gmail.com>
2019-05-23 19:55:18 -0300

    Clase 5

--- Clase5/tablaDinamica/js/script.js
+++ Clase5/tablaDinamica/js/script.js
@@ -1,1 +1,17 @@
 console.log(personas);
+//Armar una tabla con los datos del array persona
+
+addEventListener("load", leerEventos, false);
+
+function leerEventos()
+{
+    var boton = document.getElementById("btnCargar");
+    boton.addEventListener("click", cargarDatos, false);
+}
+
+function cargarDatos()
+{
+    var div = document.getElementById("info");
+    var tabla = document.createElement("table");
+    div.appendChild(tabla);
+}
