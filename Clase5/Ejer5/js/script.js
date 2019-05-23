7c7f4df92de91975dda1b035831212240ded8e5d
patoattie <patoattie@gmail.com>
2019-05-23 18:53:10 -0300

    Clase 5

---
+++ Clase5/Ejer5/js/script.js
@@ -0,0 +1,12 @@
+var boton;
+
+addEventListener("load", function()
+{
+    boton = document.getElementById("btnRun");
+    boton.addEventListener("click", ejecutar, false);
+}, false);
+
+function ejecutar()
+{
+    
+}
