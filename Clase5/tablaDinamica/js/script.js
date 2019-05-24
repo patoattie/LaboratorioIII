41b4f7b813af218af7b544047e5843d7f16c9fcc
Patricio Attie <patoattie@gmail.com>
2019-05-24 14:21:40 -0300

    Tabla dinamica

--- Clase5/tablaDinamica/js/script.js
+++ Clase5/tablaDinamica/js/script.js
@@ -6,12 +6,30 @@ addEventListener("load", leerEventos, false);
 function leerEventos()
 {
     var boton = document.getElementById("btnCargar");
-    boton.addEventListener("click", cargarDatos, false);
+    boton.addEventListener("click", crearTabla, false);
 }
 
-function cargarDatos()
+function crearTabla()
 {
     var div = document.getElementById("info");
     var tabla = document.createElement("table");
+    tabla.setAttribute("border", "1px");
     div.appendChild(tabla);
+    crearCabecera(tabla);
+}
+
+function crearCabecera(tabla)
+{
+    var filaCabecera = document.createElement("tr");
+    var atributo;
+    var columna;
+    var texto;
+    tabla.appendChild(filaCabecera);
+    for(atributo in personas[0])
+    {
+        columna = document.createElement("th");
+        filaCabecera.appendChild(columna);
+        texto = document.createTextNode(atributo);
+        columna.appendChild(texto);
+    }
 }
