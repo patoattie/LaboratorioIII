cf933e025b04d652ae86d4a8fdb00fb02f01443c
patoattie <patoattie@gmail.com>
2019-05-30 17:17:53 -0300

    Clase 6

--- Clase6/js/script.js
+++ Clase6/js/script.js
@@ -1,4 +1,17 @@
 function ejecutar()
 {
-    
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() //0 al 4 son los estados, 4 es el estado DONE
+    {
+        if(this.readyState == XMLHttpRequest.DONE) //XMLHttpRequest.DONE = 4
+        {
+            if(this.status == 200) // Estado OK
+            {
+                document.getElementById("info").innerHTML = this.responseText; //Archivo de texto del servidor
+            }
+        }
+    };
+
+    xhr.open("GET", "localhost:3000/lista.txt", true);
 }
