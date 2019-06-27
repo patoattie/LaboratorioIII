afba7bb7eabd41a8a7b78a03ad61b31cc9ce807d
patoattie <patoattie@gmail.com>
2019-06-27 18:02:37 -0300

    CLase 8

--- Clase8/Ejer1/JQ/ej1/js/script.js
+++ Clase8/Ejer1/JQ/ej1/js/script.js
@@ -1,5 +1,44 @@
 //jquery(document); //referencia a la página
-$(document).ready(function()
+//$(document) //$ es una abreviatura de jquery
+
+$(document).ready(inicializarManejadores);
+
+function inicializarManejadores()
 {
-    alert("Hola Mundo");
-}); //$ es una abreviatura de jquery
+    $("#miH1").click(function ()
+    {
+        //alert("Click en H1");
+       $(this).css("color", "red");
+       $(this).text("OTRO TEXTO");
+       //alert($(this).text());
+    });
+
+    $("#miH1").dblclick(function ()
+    {
+        //alert("Click en H1");
+       $(this).css("color", "black");
+       $(this).text("HOLA MUNDO");
+    });
+
+    var parrafos = $("p");
+    
+    parrafos.click(cambiarTexto);
+    $("#p3").attr("class", "rojos");
+
+    var elementosRojos = $(".rojos");
+
+    elementosRojos.click(function ()
+    {
+        $(this).css("color", "red");
+    });
+
+    $("div").html("<input type='text'>");
+
+    alert($("#p1").attr("id"));
+
+}
+
+function cambiarTexto()
+{
+    $(this).text("Me cambiaron");
+}
