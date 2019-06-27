5d0d0a2cc65175546a5d2cd412d4b62fa96c586c
patoattie <patoattie@gmail.com>
2019-06-27 16:16:40 -0300

    CLase 8

--- Clase8/Ejer1/reduce/app.js
+++ Clase8/Ejer1/reduce/app.js
@@ -1,10 +1,26 @@
 var vec = [5,4,3,6,7,3,2];
 
-var contador = 0;
+var personas = [{nombre = 'Juan', edad = 20},{nombre = 'Lucia',edad = 23},{nombre = 'Pedro',edad = 30}];
+
+/*var contador = 0;
 
 for(var i = 0; i < vec.length; i++)
 {
     contador += vec[i];
 }
 
-console.log("Total: " + contador);
+console.log("Total: " + contador);*/
+
+var x = vec.reduce(function (previo, actual, indice)
+{
+    console.log("previo: " + previo + " actual: " + actual + " indice: " + indice);
+    return previo + actual;
+}, 0);
+
+/*var totalEdad = personas.reduce(function (prev, actual)
+{
+    prev.total += actual.edad;
+    return prev;
+}, {total:0});
+
+console.log(prev);*/
